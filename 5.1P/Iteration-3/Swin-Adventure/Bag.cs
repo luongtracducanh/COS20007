e4cac9bfd756f63b5aeeb78fd22e739f1f18890a
Luong Trac Duc Anh <94946568+luongtracducanh@users.noreply.github.com>
2022-06-13 20:28:41 +0700

    push

---
+++ 5.1P/Iteration-3/Swin-Adventure/Bag.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Swin_Adventure
+{
+    public class Bag : Item
+    {
+        Inventory _inventory;
+
+        public Bag(string[] ids, string name, string desc) : base(ids, name, desc)
+        {
+            _inventory = new Inventory();
+        }
+
+        public GameObject Locate(string id)
+        {
+            if (AreYou(id))
+            {
+                return this;
+            }
+            return _inventory.Fetch(id);
+        }
+
+        public string FullDescription
+        {
+            get => "A bag endorned with a six-sided star inside a circle";
+        }
+
+        public Inventory Inventory
+        {
+            get => _inventory;
+        }
+    }
+}
---
+++ 5.1P/Iteration-3/Swin-Adventure/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Swin_Adventure
+{
+    public class Item : GameObject
+    {
+        public Item(string[] idents, string name, string desc) : base(idents, name, desc)
+        {
+        }
+    }
+}
