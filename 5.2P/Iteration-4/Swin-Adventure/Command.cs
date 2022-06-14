478ff3221790f5a58f57bde1492b2140f232df2d
Luong Trac Duc Anh <94946568+luongtracducanh@users.noreply.github.com>
2022-06-14 08:40:11 +0700

    push

---
+++ 5.2P/Iteration-4/Swin-Adventure/Command.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Swin_Adventure
+{
+    public abstract class Command : IdentifiableObject
+    {
+        public Command(string[] ids) : base(ids)
+        {
+        }
+
+        public abstract string Execute(Player p, string[] text);
+    }
+}
---
+++ 5.2P/Iteration-4/Swin-Adventure/Look.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Swin_Adventure
+{
+    public class Look : Command
+    {
+        public Look() :
+            base(new string[] { "look" })
+        {
+        }
+
+        public override string Execute(Player p, string[] text)
+        {
+            if (text[0].ToLower() != "look")
+                return "Error in look input.";
+
+            if (text[1].ToLower() != "at")
+                return "What do you want to look at?";
+
+            if (text.Length == 5)
+            {
+                if (text[3] != "in")
+                {
+                    return "What do you want to look in?";
+                }
+            }
+
+            IHaveInventory _container;
+
+            switch (text.Length)
+            {
+                case 3:
+                    _container = p as IHaveInventory;
+                    break;
+
+                case 5:
+                    _container = FetchContainer(p, text[4]);
+                    break;
+
+                default:
+                    _container = null;
+                    break;
+            }
+
+            string _itemid = text[2];
+            if (_container == null)
+                return "Could not find " + text[4] + ".";
+            return LookAtIn(_itemid, _container);
+        }
+
+        private IHaveInventory FetchContainer(Player p, string containerId)
+        {
+            return p.Locate(containerId) as IHaveInventory;
+        }
+
+        private string LookAtIn(string thingId, IHaveInventory container)
+        {
+            if (container.Locate(thingId) != null)
+                return container.Locate(thingId).LongDescription;
+
+            return "Could not find " + thingId + ".";
+        }
+    }
+}
