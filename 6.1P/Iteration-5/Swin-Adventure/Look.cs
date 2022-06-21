f30d238f94b8075ab9f5b4c4242b673e07ab366b
Luong Trac Duc Anh <94946568+luongtracducanh@users.noreply.github.com>
2022-06-21 21:56:08 +0700

    push

---
+++ 6.1P/Iteration-5/Swin-Adventure/Look.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Swin_Adventure
+{
+    public class Look : Command
+    {
+        public Look() :
+            base(new string[] { "look" })
+        {
+        }
+
+        public override string Execute(Player p, string[] text)
+        {
+            IHaveInventory _container;
+            string _itemid;
+            string error = "Error in look input.";
+
+            if (text[0].ToLower() != "look")
+                return error;
+
+            switch (text.Length)
+            {
+                case 3:
+                    if (text[1].ToLower() != "at")
+                        return "What do you want to look at?";
+                    _container = p;
+                    _itemid = text[2];
+                    break;
+
+                case 5:
+                    _container = FetchContainer(p, text[4]);
+                    if (_container == null)
+                        return "Could not find " + text[4] + ".";
+                    _itemid = text[2];
+                    break;
+
+                default:
+                    return error;
+            }
+            return LookAtIn(_itemid, _container);
+        }
+
+        private IHaveInventory FetchContainer(Player p, string containerId)
+        {
+            return p.Locate(containerId) as IHaveInventory;
+        }
+
+        private string LookAtIn(string thingId, IHaveInventory container)
+        {
+            if (container.Locate(thingId) != null)
+                return container.Locate(thingId).LongDescription;
+
+            return "Could not find " + thingId + ".";
+        }
+    }
+}
