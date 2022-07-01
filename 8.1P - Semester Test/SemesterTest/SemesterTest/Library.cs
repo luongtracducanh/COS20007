31dfb52f9e260f7adcaf5d5e4147222b9a78431c
Luong Trac Duc Anh <94946568+luongtracducanh@users.noreply.github.com>
2022-07-01 19:23:50 +0700

    push

---
+++ 8.1P - Semester Test/SemesterTest/SemesterTest/Library.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SemesterTest
+{
+    internal class Library
+    {
+        List<LibraryResource> _resources;
+
+        public Library(string name)
+        {
+            _resources = new List<LibraryResource>();
+        }
+
+        public void AddResource(LibraryResource resource)
+        {
+            _resources.Add(resource);
+        }
+
+        public bool HasResource(string name)
+        {
+            foreach (LibraryResource resource in _resources)
+            {
+                if (resource.Name == name && resource.OnLoan == false)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ 8.1P - Semester Test/SemesterTest/SemesterTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace SemesterTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Library library = new Library("Swinburne Library");
+            Game pubg = new Game("PlayerUnknown's Battlegrounds", "KRAFTON, PUBG Corporation", "4/5");
+            Game fn = new Game("Fortnite", "Epic Games, People Can Fly", "3/5");
+            Book doawk = new Book("Diary of a Wimpy Kid", "Jeff Kinney", "1439582637");
+            Book md = new Book("Moby-Dick", "Herman Melville", "1503280780");
+
+            library.AddResource(pubg);
+            library.AddResource(fn);
+            library.AddResource(doawk);
+            library.AddResource(md);
+
+            pubg.OnLoan = true;
+            md.OnLoan = true;
+
+            Console.WriteLine("Fortnite is not on loan, should print true.");
+            Console.WriteLine(library.HasResource("Fortnite"));
+            Console.WriteLine("Moby-Dick is on loan, should print false.");
+            Console.WriteLine(library.HasResource("Moby-Dick"));
+            Console.ReadKey();
+        }
+    }
+}
